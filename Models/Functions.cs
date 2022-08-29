d197871a627f7e1fff22d1c2b386a2c7cd707cf4
PreetiVarshney <84656806+PreetiVarshney@users.noreply.github.com>
2022-08-29 11:02:14 +0530

    Add files via upload

---
+++ Models/Functions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace hotel.Models
+{
+    public class Functions
+    {
+        public SqlConnection con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private string ConStr;
+        private SqlDataAdapter sda;
+        public int setData(string Query)
+        {
+            int Cnt;
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            Cmd.CommandText = Query;
+            Cnt = Cmd.ExecuteNonQuery();
+            con.Close();
+            return Cnt;
+        }
+
+        public DataTable GetData(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, ConStr);
+            sda.Fill(dt);
+            return dt;
+
+        }
+       
+    }
+        
+}
